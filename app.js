1fcf0a96e3aa6f662c6a3abecdca15e1ca868b3b
Eve D <evedenison@gmail.com>
2017-01-18 14:12:37 -0800

    created functionality to check if image is duplicate

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var el1 = document.getElementById('left');
 var el2 = document.getElementById('center');
 var el3 = document.getElementById('right');
 var elArray = [el1, el2, el3];
+var currentImageDis = [];
 
 function Product (name){
 
@@ -15,7 +16,7 @@ function Product (name){
   this.numClicked = 0;
 
 }
-
+//create product images and push into productArray
 for(var i = 0; i < images.length; i++){
   var newProduct = new Product(images[i]);
   productArray.push(newProduct);
@@ -29,13 +30,33 @@ function randomImageGen (){
 }
 
 console.log(randomImageGen());
-//creates random number three times, different everytime, use number to grab filepath out of productArray at index randomImageGen
+//creates random number three times, different everytime, use number to grab filepath out of productArray at index randomImageGen, and displays image
 function displayImage (){
   for(var j = 0; j < 3; j++){
     var newImage = randomImageGen();
     var getImage = productArray[newImage].filePath;
     console.log(getImage);
-    elArray[j].setAttribute('src', getImage);
+    // currentImageDis.push(getImage[0]);
+    if (j === 0){
+      currentImageDis.push(getImage[0]);
+    } else {
+      j--;
+    }
+    if (j === 1 || j === currentImageDis[0]) {
+      currentImageDis.push(getImage);
+    } else {
+      j--;
+    }
+    if (j === 2 || currentImageDis[1]) {
+      currentImageDis.push(getImage);
+    } else {
+      j--;
+    }
   }
 }
+
+function
+// elArray[j].setAttribute('src', getImage);
 displayImage();
+
+//functionality to check if it is a duplicate
